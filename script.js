eef3fe891c46354393ec308e0510524380b0c404
mannemsaiteja82-svg <mannemsaiteja82@gmail.com>
2026-06-14 12:59:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
-// Initialize ScrollReveal Configuration Engine with standard properties
-const scrollEngine = ScrollReveal({
-    distance: '40px',
-    duration: 900,
-    delay: 100,
-    reset: true // Forces elements to slide back smoothly when scrolling across section nodes
+
+var typed= new Typed(".multiple-text",{
+    strings: [
+        "Cloud and devops",
+        "Containerization",
+        "Scripting & Programming",
+        "AI & ML Enthusiast",
+    ],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay: 1000,
+    loop:true
 });
 
-// Structural Targeting Binding Rulesets
-scrollEngine.reveal('.reveal-left', { origin: 'left' });
-scrollEngine.reveal('.reveal-right', { origin: 'right' });
-scrollEngine.reveal('.reveal-bottom', { origin: 'bottom' });
 
+
+// var typed= new Typed(".multiple-text",{
+//     strings: ["FrontEnd Developer ", "Backend Developer" , "Graphic Designer"],
+//     typeSpeed:100,
+//     backSpeed:100,
+//     backDelay: 1000,
+//     loop:true
+// })
